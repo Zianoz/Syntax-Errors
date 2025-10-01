bb3dbed820fa9910132f3263952ac23bb9d3834c
Zian <Zianpakapol@gmail.com>
2025-10-01 14:03:28 +0200

    Base structure and swagger for Chas Events

---
+++ Chas Events/Chas Events/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Chas_Events.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chas_Events.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Chas Events/Chas Events/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chas_Events.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
